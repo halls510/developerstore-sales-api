dab28f5fadedf3b9e085670fd2f38a922c7fc1f1
hallison <hallison@email.com>
2025-02-26 18:38:39 -0300

    feat: adiciona mapeamento de Firstname e Lastname na UserConfiguration

--- src/Ambev.DeveloperEvaluation.ORM/Mapping/UserConfiguration.cs
+++ src/Ambev.DeveloperEvaluation.ORM/Mapping/UserConfiguration.cs
@@ -14,6 +14,8 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
         builder.HasKey(u => u.Id);
         builder.Property(u => u.Id).HasColumnType("uuid").HasDefaultValueSql("gen_random_uuid()");
 
+        builder.Property(u => u.Firstname).IsRequired().HasMaxLength(24);
+        builder.Property(u => u.Lastname).IsRequired().HasMaxLength(24);
         builder.Property(u => u.Username).IsRequired().HasMaxLength(50);
         builder.Property(u => u.Password).IsRequired().HasMaxLength(100);
         builder.Property(u => u.Email).IsRequired().HasMaxLength(100);
