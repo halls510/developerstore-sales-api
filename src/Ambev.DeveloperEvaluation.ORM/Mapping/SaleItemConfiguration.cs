fce55cdfbe5e0fb64b655abc0684fe2eadb5206e
hallison <hallison@email.com>
2025-03-01 06:57:24 -0300

    feat: ajustes na modelagem das entidades Sale e SaleItem e inclusão no DefaultContext
    - Adicionado o nome do cliente na entidade Sale para descrição denormalizada.
    - Incluído ID no SaleItem para rastreamento e referência aprimorada.
    - Ajustado relacionamento entre Sale e SaleItem garantindo integridade dos dados.
    - Implementado suporte para filial, produtos, quantidades, preços, descontos e status de cancelamento da venda.
    - Adicionada configuração da entidade Sales no DefaultContext para integração com o banco de dados.
    - Garantido suporte para mapeamento correto das vendas e seus itens no EF Core.

--- src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleItemConfiguration.cs
+++ src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleItemConfiguration.cs
@@ -1,25 +1,28 @@
-﻿using Ambev.DeveloperEvaluation.Domain.Entities;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ambev.DeveloperEvaluation.Domain.Entities;
 
 namespace Ambev.DeveloperEvaluation.ORM.Mapping;
 
-/// <summary>
-/// Configuration for SaleItem entity in Entity Framework Core.
-/// </summary>
 public class SaleItemConfiguration : IEntityTypeConfiguration<SaleItem>
 {
     public void Configure(EntityTypeBuilder<SaleItem> builder)
     {
         builder.ToTable("SaleItems");
 
-        builder.HasKey(si => new { si.SaleId, si.ProductId }); // chave composta
+        builder.HasKey(si => si.Id);
+        builder.Property(si => si.Id).HasColumnType("uuid").HasDefaultValueSql("gen_random_uuid()");
 
         builder.Property(si => si.SaleId).IsRequired();
         builder.Property(si => si.ProductId).IsRequired();
         builder.Property(si => si.ProductName).IsRequired().HasMaxLength(100);
+        builder.Property(si => si.UnitPrice).HasColumnType("decimal(18,2)").IsRequired();
         builder.Property(si => si.Quantity).IsRequired();
-        builder.Property(si => si.UnitPrice).IsRequired().HasColumnType("decimal(18,2)");
-        builder.Property(si => si.Discount).HasColumnType("decimal(18,2)");
+        builder.Property(si => si.Discount).HasColumnType("decimal(18,2)").IsRequired();
+
+        builder.HasOne<Sale>()
+               .WithMany(s => s.Items)
+               .HasForeignKey(si => si.SaleId)
+               .OnDelete(DeleteBehavior.Cascade);
     }
 }
