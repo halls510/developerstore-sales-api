4d40c91570d72320e8351dc7eb805626f312fdde
hallison <hallison@email.com>
2025-03-10 21:18:27 -0300

    feat(carts): atualiza permissões baseadas em roles para endpoints na controller CartsController

---
+++ src/Ambev.DeveloperEvaluation.Application/Carts/GetCartById/GetCartByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using Ambev.DeveloperEvaluation.Application.Carts.Common;
+using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Carts.GetCartById;
+
+/// <summary>
+/// Query to retrieve a cart by its ID
+/// </summary>
+public record GetCartByIdQuery : IRequest<CartDto>
+{
+    /// <summary>
+    /// The unique identifier of the cart
+    /// </summary>
+    public int Id { get; init; }
+}
---
+++ src/Ambev.DeveloperEvaluation.Application/Carts/GetCartById/GetCartResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace Ambev.DeveloperEvaluation.Application.Carts.GetCartById
+{
+    internal class GetCartResponse
+    {
+    }
+}
